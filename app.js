327aea1e32f74289bbe7b7bd83ef8eceee9f0675
yjh-1008 <exesea12@gmail.com>
2022-06-14 23:59:04 +0900

    using Types

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('Yout code gose here');
+function add(n1, n2) {
+    if (typeof (n1) !== 'number' || typeof (n2) !== 'number') {
+        throw new Error('Mismatch Type Error!');
+    }
+    return n1 + n2;
+}
+var number1 = '5';
+var number2 = 2.8;
+var result = add(number1, number2);
+console.log(result);
--- app.ts
+++ app.ts
@@ -1,1 +1,11 @@
-console.log('Yout code gose here');
+function add(n1:number, n2:number):number{
+    if(typeof(n1) !== 'number' || typeof(n2) !== 'number'){
+        throw new Error('Mismatch Type Error!');
+    }
+    return n1+n2;
+}
+
+const number1 = '5';
+const number2 = 2.8;
+const result = add(number1, number2);
+console.log(result)
