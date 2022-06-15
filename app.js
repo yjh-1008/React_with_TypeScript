9df8c4d6aa425e3ed724db6994f8fd2aa3625b5f
yjh-1008 <exesea12@gmail.com>
2022-06-15 13:26:52 +0900

    section 2

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 function add(n1, n2) {
-    if (typeof (n1) !== 'number' || typeof (n2) !== 'number') {
-        throw new Error('Mismatch Type Error!');
-    }
     return n1 + n2;
 }
-var number1 = '5';
-var number2 = 2.8;
-var result = add(number1, number2);
-console.log(result);
+function printResult(num) {
+    console.log(num);
+}
+printResult(add(5, 12));
---
+++ union-aliase.ts
@@ -0,0 +1,14 @@
+function combine(input1:number|string, input2:number|string){
+    let result;
+    if(typeof(input1) ==='number' && typeof(input2) ==='number'){
+        result= input1+input2;
+    }else{
+        result= input1.toString()+ input2.toString();
+    }
+    return result;
+}
+
+const combinedAges = combine(30,25);
+console.log(combinedAges)
+const conbinedNames = combine('Max','Anna');
+console.log(conbinedNames)
