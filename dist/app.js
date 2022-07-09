ab7aab876f255338502d2369615ea3d6bb65a6b3
yjh-1008 <exesea12@gmail.com>
2022-07-10 03:02:36 +0900

    07-10

--- dist/app.js
+++ dist/app.js
@@ -1,27 +1,72 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function autobind(_, _2, descriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor = {
+        configurable: true,
+        get() {
+            console.log(this);
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDescriptor;
+}
 class ProjectInput {
     constructor() {
         this.templateElement = document.getElementById("project-input");
-        this.hostElement = document.getElementById('app');
-        const importNode = document.importNode(this.templateElement.content, true);
-        this.element = importNode.firstElementChild;
+        this.hostElement = document.getElementById("app");
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
         this.element.id = 'user-input';
         this.titleInputElement = this.element.querySelector('#title');
-        this.descriptionElement = this.element.querySelector('#description');
+        this.descriptionInputElement = this.element.querySelector('#description');
         this.peopleInputElement = this.element.querySelector('#people');
         this.configure();
         this.attach();
     }
+    clearInput() {
+        this.titleInputElement.value = '';
+        this.descriptionInputElement.value = '';
+        this.peopleInputElement.value = '';
+    }
+    getUserInput() {
+        const enteredTitle = this.titleInputElement.value;
+        const enteredDescription = this.descriptionInputElement.value;
+        const enteredPeople = this.peopleInputElement.value;
+        if (enteredTitle.trim().length === 0 ||
+            enteredDescription.trim().length === 0 ||
+            enteredPeople.trim().length === 0) {
+            alert('invaild input');
+            return;
+        }
+        else {
+            return [enteredTitle, enteredDescription, +enteredPeople];
+        }
+    }
     submitHandler(event) {
         event.preventDefault();
-        console.log(this.titleInputElement.value);
+        const userInput = this.getUserInput();
+        if (userInput instanceof Array) {
+            console.log("not here");
+            const [title, description, people] = userInput;
+            console.log(title, description, people);
+        }
     }
     configure() {
-        this.element.addEventListener('submit', this.submitHandler.bind(this));
+        this.element.addEventListener('submit', this.submitHandler);
     }
     attach() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
     }
 }
+__decorate([
+    autobind
+], ProjectInput.prototype, "submitHandler", null);
 const prjInput = new ProjectInput();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,81 @@
+function autobind(_: any, _2: string,descriptor: PropertyDescriptor){
+  const originalMethod = descriptor.value;
+
+  const adjDescriptor:PropertyDescriptor ={
+    configurable:true,
+    get(){
+      console.log(this);
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    }
+  };
+  return adjDescriptor;
+}
+
+
 class ProjectInput{
-  templateElement:HTMLTemplateElement;
+  
+  templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
-  element:HTMLFormElement;
+  element: HTMLFormElement;
   titleInputElement: HTMLInputElement;
-  descriptionElement: HTMLInputElement;
+  descriptionInputElement: HTMLTextAreaElement;
   peopleInputElement: HTMLInputElement;
   constructor(){
-    
-    this.templateElement = <HTMLTemplateElement> document.getElementById("project-input")!;
-    this.hostElement = <HTMLDivElement> document.getElementById('app')!;
-    
-    const importNode = document.importNode(this.templateElement.content,true);
-    this.element = <HTMLFormElement> importNode.firstElementChild;
+    this.templateElement =<HTMLTemplateElement> document.getElementById("project-input")!;
+    this.hostElement = <HTMLDivElement> document.getElementById("app")!;
+
+    const importedNode = document.importNode(this.templateElement.content,true);
+    this.element = <HTMLFormElement> importedNode.firstElementChild;
     this.element.id = 'user-input';
-    this.titleInputElement = <HTMLInputElement> this.element.querySelector('#title');
-    this.descriptionElement = <HTMLInputElement> this.element.querySelector('#description');
-    this.peopleInputElement = <HTMLInputElement> this.element.querySelector('#people');
+    this.titleInputElement = <HTMLInputElement> this.element.querySelector('#title')!;
+    this.descriptionInputElement = <HTMLTextAreaElement> this.element.querySelector('#description')!;
+    this.peopleInputElement = <HTMLInputElement> this.element.querySelector('#people')!;
     this.configure();
     this.attach();
   }
+
+  private clearInput(){
+    this.titleInputElement.value='';
+    this.descriptionInputElement.value=''
+    this.peopleInputElement.value='';
+  }
+
+  private getUserInput():[string,string,number]|void{
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+    if(enteredTitle.trim().length === 0 ||
+     enteredDescription.trim().length ===0 ||
+      enteredPeople.trim().length===0){
+        alert('invaild input');
+        return 
+    }else{
+
+      return [enteredTitle,enteredDescription, +enteredPeople];
+    }
+  }
+
+
+
+  @autobind
   private submitHandler(event:Event){
     event.preventDefault();
-    console.log(this.titleInputElement.value);
+    const userInput = this.getUserInput();
+    if(userInput instanceof Array){
+      console.log("not here")
+      const [title,description,people] = userInput;
+      console.log(title,description,people);
+    }
   }
 
   private configure(){
-    this.element.addEventListener('submit', this.submitHandler.bind(this));
+    this.element.addEventListener('submit',this.submitHandler);
   }
-  private attach(){
-    this.hostElement.insertAdjacentElement('afterbegin', this.element);
-  }
+
+ private attach(){
+  this.hostElement.insertAdjacentElement('afterbegin',this.element);
+ }
 }
 
 const prjInput = new ProjectInput();
